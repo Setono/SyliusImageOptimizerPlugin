7b0008c17231c71c8dea4969c6435710be9b41c5
Joachim Løvgaard <joachim@loevgaard.dk>
2020-11-26 10:01:48 +0100

    Made the optimize command more robust by adding a lock mechanism and pinging kraken before trying to optimize

--- src/Command/OptimizeCommand.php
+++ src/Command/OptimizeCommand.php
@@ -4,24 +4,33 @@ declare(strict_types=1);
 
 namespace Setono\SyliusImageOptimizerPlugin\Command;
 
+use Setono\Kraken\Client\ClientInterface;
+use Setono\Kraken\Exception\RequestFailedException;
 use Setono\SyliusImageOptimizerPlugin\Message\Command\OptimizeConfiguredImageResources;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Command\LockableTrait;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
 
 final class OptimizeCommand extends Command
 {
+    use LockableTrait;
+
     /** @var string */
     protected static $defaultName = 'setono:sylius-image-optimizer:optimize';
+
+    /** @var ClientInterface */
+    private $client;
 
     /** @var MessageBusInterface */
     private $commandBus;
 
-    public function __construct(MessageBusInterface $commandBus)
+    public function __construct(ClientInterface $client, MessageBusInterface $commandBus)
     {
         parent::__construct();
 
+        $this->client = $client;
         $this->commandBus = $commandBus;
     }
 
@@ -32,6 +41,22 @@ final class OptimizeCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        if (!$this->lock()) {
+            $output->writeln('The command is already running in another process.');
+
+            return 0;
+        }
+
+        try {
+            $this->client->status();
+        } catch (RequestFailedException $e) {
+            $output->writeln('<error>An error occurred when trying to ping the Kraken API:</error>');
+            $output->writeln('<error>- ' . $e->getMessage() . '</error>');
+            $output->writeln('<comment>Usually this is because of a wrong API key or API secret</comment>');
+
+            return 1;
+        }
+
         $this->commandBus->dispatch(new OptimizeConfiguredImageResources());
 
         return 0;
